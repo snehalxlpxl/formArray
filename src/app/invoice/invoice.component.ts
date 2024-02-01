cab999283b48a9dfa5f33ccf793e755b0814b769
Snehal Shivkar <snehal@Fretlogs-MacBook-Pro.local>
2024-02-01 16:04:39 +0530

    invoice form

--- src/app/invoice/invoice.component.ts
+++ src/app/invoice/invoice.component.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatButtonModule} from '@angular/material/button';
+import {FormsModule,FormGroup,FormControl, Validators, ReactiveFormsModule} from '@angular/forms';
+
 
 @Component({
   selector: 'app-invoice',
   standalone: true,
-  imports: [],
+  imports: [FormsModule,MatButtonModule,ReactiveFormsModule, MatFormFieldModule, MatInputModule],
   templateUrl: './invoice.component.html',
   styleUrl: './invoice.component.css'
 })
 export class InvoiceComponent {
-
+rectform=new FormGroup({
+  name:new FormControl('',Validators.required),
+  comment:new FormControl('',Validators.required),
+});
+save(){
+  console.log(this.rectform.value);
+}
 }
